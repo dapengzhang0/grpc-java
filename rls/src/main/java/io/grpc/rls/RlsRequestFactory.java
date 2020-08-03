021d33a240ee0d1d14e8e49a73d87cc7fa39433b
Penn (Dapeng) Zhang <zdapeng@google.com>
2020-08-03 11:53:52 -0700

    hack

--- rls/src/main/java/io/grpc/rls/RlsRequestFactory.java
+++ rls/src/main/java/io/grpc/rls/RlsRequestFactory.java
@@ -47,7 +47,7 @@ final class RlsRequestFactory {
 
   RlsRequestFactory(RouteLookupConfig rlsConfig) {
     checkNotNull(rlsConfig, "rlsConfig");
-    this.target = rlsConfig.getLookupService();
+    this.target = rlsConfig.getValidTargets().get(0);
     this.keyBuilderTable = createKeyBuilderTable(rlsConfig);
   }
 
